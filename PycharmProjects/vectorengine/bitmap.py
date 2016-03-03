e171699e186b9bc14d757e27fd999335d33c420f
mdurrer <michael.durrer@gmail.com>
2016-03-03 07:49:19 +0100

    Bitmap.py added

---
+++ PycharmProjects/vectorengine/bitmap.py
@@ -0,0 +1,13 @@
+class Bitmap(object):
+	width, height = float(),float()
+	def __init__(self,width,height):
+		self.width, self.height = width,height
+		self.display
+	def getWidth(self):
+		return self.width
+	def setWidth(self,value):
+		self.width = value
+	def getHeight(self):
+		return self.height
+	def setHeight(self,value):
+		self.height = value
