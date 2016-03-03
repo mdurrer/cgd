2bffeb39a10bbd026d8d0e30cd622d7f0f240301
mdurrer <michael.durrer@gmail.com>
2016-03-03 12:33:26 +0100

    Bitmap.py updates

--- PycharmProjects/vectorengine/bitmap.py
+++ PycharmProjects/vectorengine/bitmap.py
@@ -1,8 +1,11 @@
+from math import *
+import numpy as np
 class Bitmap(object):
 	width, height = float(),float()
+	display = np.array([],dtype=int)
 	def __init__(self,width,height):
 		self.width, self.height = width,height
-		self.display
+		self.display = np.zeros([self.width * self.height * 4],dtype=int)
 	def getWidth(self):
 		return self.width
 	def setWidth(self,value):
