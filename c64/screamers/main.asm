79e6c949a958382989e379106fc37579cbf1e833
mdurrer <michael.durrer@gmail.com>
2016-04-21 05:27:18 +0200

    Screamers update, C64 IRQ and Relaunch64 files

--- c64/screamers/main.asm
+++ c64/screamers/main.asm
@@ -1,5 +1,56 @@
-* = $c000
-
+// Screamers example
+.pc = $0801 "Basic Upstart"
+:BasicUpstart2(start)
+.pc = $8000
+start:
+jsr $ff81
+sei
+lda #$7f
+sta $dc0d
+sta $dd0d
+lda $dc0d
+lda $dd0d
 lda #$01
-sta $d021
-rts
+sta $d01a
+lda #$71
+sta $d012
+lda #$1b
+sta $d011
+lda #$35
+sta $01
+lda #<mainirq
+ldx #>mainirq
+sta $fffe
+stx $ffff
+
+
+cli
+
+jmp *
+
+mainirq:
+pha
+txa
+pha
+tya
+pha
+inc $d021
+nop
+nop
+nop
+dec $d021
+lda #<mainirq
+ldx #>mainirq
+sta $fffe
+stx $ffff
+
+lda #$71
+sta $d012
+lda #$01
+sta $d019
+pla
+tay
+pla
+tax
+pla
+rti
