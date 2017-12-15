af1a0e38f0ae52007c4993322f1bb6267335995a
mdurrer <michael.durrer@gmail.com>
2017-12-15 07:49:26 +0100

    cpu.py and opcodes added

--- python3/aw2/cpu.py
+++ python3/aw2/cpu.py
@@ -12,8 +12,13 @@ class CPU(object):
     def __init__(self,name,adr):
         self.name = name
         self.engine = name
+        self.pc = 0
         self.video = Video(1600,900,bpp=24, flags=SDL_GL_DOUBLEBUFFER)
         print ("Virtual CPU created")
         return None
-    
+    def nextOpcode(list):
+        if self.pc == 0x00001]:
+            print ("RTS")
+            pygame.quit()
+        
     
